5f8b315161fdba5525c8dbcd1f8457d21a06631e
Abhishek Kumar <gwydionmaranio@gmail.com>
2025-09-08 00:21:10 +0530

    fixes for mongodb

--- Utils/mongo.js
+++ Utils/mongo.js
@@ -1,30 +1,34 @@
-
 import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
 
+const mongo_url = process.env.MONGO_URI;
 
-export const connectNotes = async() => {
-  await mongoose.connect(process.env.MONGO_URI,{
-    dbName: 'whatsapp_api'
+export const connectNotes = async () => {
+  await mongoose.connect(mongo_url, {
+    dbName: "whatsapp_api",
   });
 
-  let noteSchema = new mongoose.Schema({
-    note: String,
-    inChat: String,
-    savedBy: String, 
-    index: Number
-  },{collection: 'notes'})
+  let noteSchema = new mongoose.Schema(
+    {
+      note: String,
+      inChat: String,
+      savedBy: String,
+      index: Number,
+    },
+    { collection: "notes" },
+  );
 
   let Note = mongoose.model("Note", noteSchema);
   return Note;
-}
+};
 
-
-export const connectAuth = async() => {
-  console.log('connecting to auth')
-  const mongoClient = new MongoClient(process.env.MONGO_URI);
+export const connectAuth = async () => {
+  console.log("connecting to auth");
+  const mongoClient = new MongoClient(mongo_url);
   await mongoClient.connect();
-  const collection = mongoClient.db("whatsapp_api").collection("auth_info_baileys")
-  console.log('connected to auth')
+  const collection = mongoClient
+    .db("whatsapp_api")
+    .collection("auth_info_baileys");
+  console.log("connected to auth");
   return collection;
-}
+};
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { DisconnectReason, fetchLatestBaileysVersion, useMultiFileAuthState } from "@whiskeysockets/baileys";
+import {
+  DisconnectReason,
+  fetchLatestBaileysVersion,
+  useMultiFileAuthState,
+} from "@whiskeysockets/baileys";
 import { MongoClient } from "mongodb";
-import 'dotenv/config'
-import * as baileys from '@whiskeysockets/baileys';
+import "dotenv/config";
+import * as baileys from "@whiskeysockets/baileys";
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 import chalk from "chalk";
@@ -10,51 +14,66 @@ import pino from "pino";
 import getWeather from "./API_module/weatherAPI.js";
 import { connectAuth, connectNotes } from "./Utils/mongo.js";
 import useMongoDbAuthState from "./mongoDbAuthState.js";
-import { handleAddNote, handleDeleteNotes, handleGetNotes } from "./API_module/notesAPI.js";
+import {
+  handleAddNote,
+  handleDeleteNotes,
+  handleGetNotes,
+} from "./API_module/notesAPI.js";
 import { tagAll } from "./API_module/tagall.js";
+import NodeCache from "node-cache";
 
+const usePairingCode = process.argv.includes("--use-pairing-code");
+const rl = readline.createInterface({ input, output });
 
-const usePairingCode = process.argv.includes('--use-pairing-code');
-const rl = readline.createInterface({input,output});
-
-async function connectToWhatsApp () {
-
+async function connectToWhatsApp() {
   const Note = await connectNotes();
   const authCollection = await connectAuth();
-  const {version, isLatest} = await fetchLatestBaileysVersion();
-  console.log(chalk.yellow(`using WA v${version.join('.')}, isLatest: ${isLatest}`))
-  const {state, saveCreds} = await useMongoDbAuthState(authCollection); 
-  // const {state, saveCreds} = await useMultiFileAuthState("auth_info_baileys"); 
+  const { version, isLatest } = await fetchLatestBaileysVersion();
+  console.log(
+    chalk.yellow(`using WA v${version.join(".")}, isLatest: ${isLatest}`),
+  );
+  const { state, saveCreds } = await useMongoDbAuthState(authCollection);
+  // const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
+  const groupCache = new NodeCache({});
 
   const sock = baileys.makeWASocket({
     version,
-    logger: pino({level: 'silent'}),
+    logger: pino({ level: "silent" }),
     auth: state,
-    browser: baileys.Browsers.windows('Firefox'),
+    browser: baileys.Browsers.windows("Firefox"),
     printQRInTerminal: !usePairingCode,
     generateHighQualityLinkPreview: true,
-  })
+    cachedGroupMetadata: async (jid) => groupCache.get(jid),
+  });
 
-  if(usePairingCode && !sock.authState.creds.registered){
-    const phoneNumber = await rl.question("Please enter your mobile phoneNumber");
+  if (usePairingCode && !sock.authState.creds.registered) {
+    const phoneNumber = await rl.question(
+      "Please enter your mobile phoneNumber",
+    );
     const code = await sock.requestPairingCode(phoneNumber);
-    console.log(`pairing code : ${code}`)
+    console.log(`pairing code : ${code}`);
   }
 
-  sock.ev.on('connection.update', (update) => {
+  sock.ev.on("connection.update", (update) => {
     const { connection, lastDisconnect } = update;
 
-    if(connection === 'close') {
-      const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut
-      console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+      console.log(
+        "connection closed due to ",
+        lastDisconnect.error,
+        ", reconnecting ",
+        shouldReconnect,
+      );
       // reconnect if not logged out
-      if(shouldReconnect) {
-        connectToWhatsApp()
+      if (shouldReconnect) {
+        connectToWhatsApp();
       }
-    } else if(connection === 'open') {
-      console.log(chalk.greenBright('opened connection'))
+    } else if (connection === "open") {
+      console.log(chalk.greenBright("opened connection"));
     }
-  })
+  });
 
   sock.ev.on("creds.update", saveCreds);
 
@@ -62,58 +81,60 @@ async function connectToWhatsApp () {
   //   console.log( chalk.blue('\n its me\n'),m);
   // })
 
-  console.log('listening')
-  sock.ev.on('messages.upsert', async (m) => {
-    let responseData ;
+  console.log("listening");
+  sock.ev.on("messages.upsert", async (m) => {
+    let responseData;
 
-    if (m.messages[0].message){
+    if (m.messages[0].message) {
       // console.log((m.messages[0].message), ' from ', JSON.stringify(m,null,2))
-      let message = m.messages[0]?.message?.conversation || m.messages[0]?.message?.extendedTextMessage?.text;
-      if (message?.toLowerCase()?.trim()?.startsWith('@gwybot')){
-      let chatJid = m.messages[0].key.remoteJid;
-        let cmdStringArray = message?.split(' ')
-        if( cmdStringArray.length > 1 ){
-
+      let message =
+        m.messages[0]?.message?.conversation ||
+        m.messages[0]?.message?.extendedTextMessage?.text;
+      if (message?.toLowerCase()?.trim()?.startsWith("@gwybot")) {
+        let chatJid = m.messages[0].key.remoteJid;
+        let cmdStringArray = message?.split(" ");
+        if (cmdStringArray.length > 1) {
           let command = cmdStringArray[1]?.toLowerCase();
 
           console.log(command);
-          switch (command){
-            case 'weather': 
-              getWeather(cmdStringArray[2] , sock , m.messages[0].key.remoteJid );
+          switch (command) {
+            case "weather":
+              getWeather(cmdStringArray[2], sock, m.messages[0].key.remoteJid);
               break;
-            case 'addnote':
-              handleAddNote(m,Note,sock);
+            case "addnote":
+              handleAddNote(m, Note, sock);
               break;
-            case 'getnotes':
-              handleGetNotes(m,Note,sock);
+            case "getnotes":
+              handleGetNotes(m, Note, sock);
               break;
-            case 'deletenote':
-              handleDeleteNotes(m,Note,sock);
+            case "deletenote":
+              handleDeleteNotes(m, Note, sock);
               break;
-            case 'tagall':
-              tagAll(m,sock);
+            case "tagall":
+              tagAll(m, sock);
               break;
-            default : 
-              sock.sendMessage(chatJid, {text: 'Hello!,\n Gwybot here, this might not be valid command (till now atleast :) )'});
+            default:
+              sock.sendMessage(chatJid, {
+                text: "Hello!,\n Gwybot here, this might not be valid command (till now atleast :) )",
+              });
               break;
           }
-        }else {
-          sock.sendMessage(chatJid, {text: 'Hello I am GwyBot, Made By Abhishek Kumar and Ranjay Singh'});
+        } else {
+          sock.sendMessage(chatJid, {
+            text: "Hello I am GwyBot, Made By Abhishek Kumar and Ranjay Singh",
+          });
         }
       }
 
       // console.log(chalk.red('\nBoomBurst\n') , 'replying to')
       // await sock.sendMessage(m.messages[0].key.remoteJid, { text: 'Hello there!' })
-    }
-    else{
-
+    } else {
       // console.log(chalk.red('\nBoomBurst\n') , 'replying to' , JSON.stringify(m , undefined , 2 ))
       //  let userjid  = m.messages[0].key.remoteJid.includes('@s.whatsapp.net') ? m.messages[0].key.remoteJid : m.messages[0].key.participant ;
-      if(!m.messages[0].key.fromMe){
+      if (!m.messages[0].key.fromMe) {
       }
-
     }
-  })
+  });
 }
 // run in main file
-connectToWhatsApp()
+connectToWhatsApp();
